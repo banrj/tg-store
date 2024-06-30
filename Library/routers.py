b8dcda3e10bc139d22ecc6a89ffedf0cea738da9
Bogomolov Daniil <105605702+banrj@users.noreply.github.com>
2024-06-30 21:59:58 +0200

    Delete Library directory

--- Library/routers.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from handlers.book.endpoints import book_router
-from handlers.price_history.endpoints import price_history_router
-
-main_api_router = APIRouter(prefix='/api/v1')
-
-main_api_router.include_router(book_router, prefix='/books', tags=['books'])
-main_api_router.include_router(price_history_router, prefix='/price-history', tags=['price_history'])
