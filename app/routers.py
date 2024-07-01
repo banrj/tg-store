71c00fe2d63d8ab296cf7f9d40844ecb628dc9a3
banrj <eh.ehr@bk.ru>
2024-07-01 21:54:02 +0200

    webhook + api gw

--- app/routers.py
+++ app/routers.py
@@ -4,6 +4,7 @@ from aiogram.types import Update
 
 from app.common import TelegramApp
 from app.config import settings
+from app.core.log_config import logger
 
 main_router = APIRouter()
 
@@ -13,7 +14,15 @@ bot = Bot(settings.TG_KEY)
 @main_router.post('/tg-webhook', tags=['telegram'], response_model=None)
 async def telegram_webhook(request: Request, tg_app: TelegramApp) -> Response:
     """Handle incoming Telegram updates by putting them into the `update_queue`"""
-    json_data = await request.json()
-    update = Update.de_json(data=json_data)
-    await tg_app.feed_update(update=update, bot=bot)
-    return Response()
+    try:
+        json_data = await request.json()
+        logger.info(f"Received update: {json_data}")
+
+        update = Update.de_json(data=json_data)
+        await tg_app.feed_update(update=update, bot=bot)
+
+        logger.info("Update processed successfully")
+        return Response(status_code=200)
+    except Exception as e:
+        logger.error(f"Error handling update: {e}")
+        return Response(status_code=500, content=str(e))
