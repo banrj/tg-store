5e14102de765f2de1406ed1b9e20ad5ac475aa5e
banrj <eh.ehr@bk.ru>
2024-07-05 02:11:34 +0200

    more log

--- app/routers.py
+++ app/routers.py
@@ -11,18 +11,18 @@ main_router = APIRouter()
 bot = Bot(settings.TG_KEY)
 
 
-@main_router.post('/tg-webhook', tags=['telegram'], response_model=None)
+@main_router.post('/tgwebhook', tags=['telegram'], response_model=None)
 async def telegram_webhook(request: Request, tg_app: TelegramApp) -> Response:
     """Handle incoming Telegram updates by putting them into the `update_queue`"""
     try:
         json_data = await request.json()
         logger.info(f"Дернули rout: {json_data}")
 
-        update = Update.de_json(data=json_data)
+        update = Update.model_validate(await request.json(), context={"bot": bot})
         await tg_app.feed_update(update=update, bot=bot)
 
         logger.info("Update processed successfully")
-        return Response(status_code=200)
+        return Response(status_code=200, content='Update передан боту')
     except Exception as e:
         logger.error(f"Что-то в роуте: {e}")
         return Response(status_code=500, content=str(e))
